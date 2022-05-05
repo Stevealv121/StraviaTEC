391740e2505b92236ea347e36cadd65e2a491acf
Steve <stevealv@hotmail.com>
2022-05-05 15:52:51 -0600

    [Add] Sign-up view

--- StraviaTEC-Web-App/src/app/app-routing.module.ts
+++ StraviaTEC-Web-App/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "/login" },
-  { path: "login", component: LoginComponent }
+  { path: "login", component: LoginComponent },
+  { path: "sign-up", component: SignUpComponent }
 ];
 
 @NgModule({
---
+++ StraviaTEC-Web-App/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UserI } from '../models/user.interface';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+  url: string | ArrayBuffer | null | undefined = '';
+
+  constructor(private api: ApiService) { }
+
+  registerForm = new FormGroup({
+    nationality: new FormControl(''),
+    username: new FormControl(''),
+    firstName: new FormControl(''),
+    secondName: new FormControl(''),
+    firstSurname: new FormControl(''),
+    secondSurname: new FormControl(''),
+    profilePicture: new FormControl(''),
+    level: new FormControl(''),
+    password: new FormControl(''),
+    birthDate: new FormControl('')
+  })
+  selectedOption: any;
+  levelOptions: string[] = ["Beginner", "Intermediate", "Elite"];
+
+  ngOnInit(): void {
+    // this.url = '../../assets/images/avatar.png';
+  }
+
+  postForm(form: UserI) {
+    this.api.signUp(form).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+
+  onSelectFile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target?.result;
+      }
+    }
+  }
+
+}
