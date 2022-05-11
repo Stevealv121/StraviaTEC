953afb79bc8c97c39eb8ca972ebba89ea70e595e
Steve <stevealv@hotmail.com>
2022-05-11 09:14:20 -0600

    [Add] Show all comments + profile picture

--- StraviaTEC-Web-App/src/app/home/home.component.ts
+++ StraviaTEC-Web-App/src/app/home/home.component.ts
@@ -11,8 +11,23 @@ export class HomeComponent implements OnInit {
   routeMap: string = "assets/images/route-map.png";
   friendsActivity: number[] = [1, 2, 3];
   friendImage: string = "assets/images/avatar.png";
+  isVisible: boolean = false;
+  topComments: number[] = [1, 2];
+  hasComments: boolean = false;
+  comments: number[] = [1, 2, 3, 4];
 
   ngOnInit(): void {
+    this.checkIfHasComments();
   }
 
+  showComments() {
+    this.isVisible = true;
+  }
+  showLessComments() {
+    this.isVisible = false;
+  }
+
+  checkIfHasComments() {
+    !this.topComments.length ? this.hasComments = false : this.hasComments = true;
+  }
 }
