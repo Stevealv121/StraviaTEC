efda08ff81b86d09d3d3513ca2f42b57a0dede2f
Dennis Jimenez <alejimenezc@hotmail.com>
2022-05-16 18:54:24 -0600

    [Add] Google Maps

--- StraviaTEC_Mobile/app/src/main/java/com/example/straviatec_mobile/MainActivity.java
+++ StraviaTEC_Mobile/app/src/main/java/com/example/straviatec_mobile/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.straviatec_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent myintent = new Intent(MainActivity.this,Map.class);
+        startActivity(myintent);
     }
 }
---
+++ StraviaTEC_Mobile/app/src/main/java/com/example/straviatec_mobile/Map.java
@@ -0,0 +1,27 @@
+package com.example.straviatec_mobile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback{
+    SupportMapFragment mapFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
