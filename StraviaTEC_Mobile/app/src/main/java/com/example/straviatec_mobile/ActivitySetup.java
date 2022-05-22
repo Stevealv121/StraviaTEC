193d5a99f693b4a3f951fb272929d5ce91b6e83b
Dennis Jimenez <alejimenezc@hotmail.com>
2022-05-21 19:45:40 -0600

    [Update] Activity select

--- StraviaTEC_Mobile/app/src/main/java/com/example/straviatec_mobile/ActivitySetup.java
+++ StraviaTEC_Mobile/app/src/main/java/com/example/straviatec_mobile/ActivitySetup.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ActivitySetup extends AppCompatActivity {
+
+    private Spinner activities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        activities = findViewById(R.id.activities);
+
+        List<String> states = Arrays.asList("Select Sport or Activity","Running","Swimming","Cycling","Trekking","Kayak","Hiking");
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item,states);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        activities.setAdapter(adapter);
     }
 
     public void onClick(View view) {
