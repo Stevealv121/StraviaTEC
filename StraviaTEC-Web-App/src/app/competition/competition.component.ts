8ea6f6b6379d3f23a681362389f2825556332811
Sebas <sebas0163@hotmail.com>
2022-05-28 23:57:31 -0600

    [Add] Get's structure for the organizer view (no functional))

--- StraviaTEC-Web-App/src/app/competition/competition.component.ts
+++ StraviaTEC-Web-App/src/app/competition/competition.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Race } from '../models/race';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-competition',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompetitionComponent implements OnInit {
 
-  constructor() { }
+  races:Race[];
+
+  constructor(private api:ApiService) {
+    this.races=[]
+   }
 
   ngOnInit(): void {
+    //this.loadRaces()
+  }
+
+  loadRaces(){
+    this.api.getRacesByManagerId("dennis").subscribe((data:any)=>{ //change dennis for the username
+      this.races = data;
+    })
   }
 
 }
--- StraviaTEC-Web-App/src/app/services/api.service.ts
+++ StraviaTEC-Web-App/src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { UserI } from '../models/user.interface';
 import { ResponseI } from '../models/response.interface';
@@ -12,8 +12,16 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  url: string = "http://localhost:5104/api/";
+  url: string = "https://localhost:7060/api/";
   userPath: string = this.url + "User";
+  getgroupInfoPath: string = this.url + "Group/ByManager/";
+  getRaceManagerId: string = this.url + "Race/ByManagerId/";
+  getChallengeManagerId: string = this.url + "Challenge/ByManagerId/";
+  getSportsPath: string = this.url + "Sport";
+  getSponsorPath:string = this.url + "Sponsor";
+  getCategoryPath:string = this.url + "Category";
+
+
 
   login(userName: string, password: string): Observable<UserI> {
 
@@ -24,5 +32,59 @@ export class ApiService {
   signUp(form: UserI): Observable<ResponseI> {
     return this.http.post<ResponseI>(this.userPath, form)
   }
+
+  //POST
+
+  //GETS
+
+  /**
+   * This function asks the api for the groups created by username
+   * @param username owner's username
+   * @returns groups who belongs to the specified user
+   */
+  getGroupInfoByManagerId(username:string){
+    return this.http.get<string[]>(this.getgroupInfoPath+username); //some problems maybe the http or https
+  }
+  /**
+   * This function asks the api for the races created by the user.
+   * @param username owner's username
+   * @returns races created by the specified user
+   */
+  getRacesByManagerId(username:string){
+    return this.http.get<string[]>(this.getRaceManagerId+username);
+  }
+  /**
+   * This function asks the api for the challenges created by the user.
+   * @param username owner's username
+   * @returns Challenges created by the specified user
+   */
+  getChallengeByManagerId(username:string){
+    return this.http.get<string[]>(this.getChallengeManagerId+username);
+  }
+  /**
+   * This function asks the api for the sports in the data base
+   * @returns sports in the data base
+   */
+  getSports(){
+    return this.http.get<string[]>(this.getSportsPath);
+  }
+  /**
+   This function asks the api for the sponsors in the data base
+   * @returns sponsors in the data base
+   */
+  getSponsor(){
+    return this.http.get<string[]>(this.getSponsorPath);
+  }
+  /**
+   This function asks the api for the categories in the data base
+   * @returns categories in the data base
+   */
+  getCategory(){
+    return this.http.get<string[]>(this.getCategoryPath);
+  }
+  //DELETES
+
+  //PUTS
+
 
 }
