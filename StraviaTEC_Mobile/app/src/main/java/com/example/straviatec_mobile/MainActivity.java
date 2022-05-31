8945df0ed3b86b41addd471d828190957524ee22
Dennis Jimenez <alejimenezc@hotmail.com>
2022-05-30 20:15:49 -0600

    [Update] Connection fixes

--- StraviaTEC_Mobile/app/src/main/java/com/example/straviatec_mobile/MainActivity.java
+++ StraviaTEC_Mobile/app/src/main/java/com/example/straviatec_mobile/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.straviatec_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.straviatec_mobile.Entities.Activity;
+import com.example.straviatec_mobile.Entities.User;
 import com.example.straviatec_mobile.Interfaces.ActivityAPI;
+import com.example.straviatec_mobile.Interfaces.UserAPI;
+import com.example.straviatec_mobile.Utilities.Utilities;
 
 import java.util.List;
 
@@ -30,22 +36,75 @@ public class MainActivity extends AppCompatActivity {
     }
     private void sincA() {
         SQLitehelper conn = new SQLitehelper(this, "StraviaTEC_DB", null,1);
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://10.0.2.2:7060/")
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://10.0.2.2:7060/")
                 .addConverterFactory(GsonConverterFactory.create()).build();
         ActivityAPI activityAPI = retrofit.create(ActivityAPI.class);
         Call<List<Activity>> call = activityAPI.findA();
         call.enqueue(new Callback<List<Activity>>() {
             @Override
             public void onResponse(Call<List<Activity>> call, Response<List<Activity>> response) {
+                try{
+                    List<Activity> activityList = response.body();
+                    for(Activity a: activityList){
+                        SQLiteDatabase db = conn.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.put(Utilities.FIELD_USERNAME, a.getUsername());
+                        values.put(Utilities.FIELD_ID, a.getId());
+                        values.put(Utilities.FIELD_DATE, a.getDate());
+                        values.put(Utilities.FIELD_DURATION, a.getDuration());
+                        values.put(Utilities.FIELD_MILEAGE, a.getMileage());
+                        values.put(Utilities.FIELD_ROUTE, a.getRoute());
+                        values.put(Utilities.FIELD_SPORTNAME, a.getSportName());
+
+                        db.insert(Utilities.TABLE_ACTIVITY, null, values);
+                        db.close();
+                    }
+                }catch(Exception ex){
+                    Toast.makeText(MainActivity.this,ex.getMessage(),Toast.LENGTH_SHORT).show();
+                }
             }
 
             @Override
             public void onFailure(Call<List<Activity>> call, Throwable t) {
-
+                Toast.makeText(MainActivity.this,"Connection Error",Toast.LENGTH_SHORT).show();
             }
         });
     }
     private void sincU() {
+        SQLitehelper conn = new SQLitehelper(this,"StraviaTEC_DB",null,1);
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://10.0.2.2:7060/")
+                .addConverterFactory(GsonConverterFactory.create()).build();
+        UserAPI userAPI = retrofit.create(UserAPI.class);
+        Call<List<User>> call = userAPI.findU();
+        call.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                try{
+                    List<User> userList = response.body();
+                    for(User u: userList){
+                        SQLiteDatabase db = conn.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.put(Utilities.FIELD_USERNAME, u.getUserName());
+                        values.put(Utilities.FIELD_FNAME, u.getFirstName());
+                        values.put(Utilities.FIELD_SNAME, u.getSecondName());
+                        values.put(Utilities.FIELD_FSNAME, u.getFirstSurname());
+                        values.put(Utilities.FIELD_SSNAME, u.getSecondSurname());
+                        values.put(Utilities.FIELD_PASS, u.getPassword());
+                        values.put(Utilities.FIELD_LEVEL, u.getLevel());
+                        values.put(Utilities.FIELD_PROFILEPIC, u.getProfilePicture());
+
+                        db.insert(Utilities.TABLE_USER, null,values);
+                        db.close();
+                    }
+                }catch(Exception ex){
+                    Toast.makeText(MainActivity.this,ex.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                Toast.makeText(MainActivity.this,"Connection Error",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
