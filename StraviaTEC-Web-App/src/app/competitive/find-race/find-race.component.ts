89f982068dd254318bc187dca86301b8a4310728
Steve <stevealv@hotmail.com>
2022-05-31 21:51:51 -0600

    [Add] Race inscription

--- StraviaTEC-Web-App/src/app/competitive/find-race/find-race.component.ts
+++ StraviaTEC-Web-App/src/app/competitive/find-race/find-race.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RaceI } from 'src/app/models/race.interface';
 import { ApiService } from 'src/app/services/api.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-find-race',
@@ -10,15 +11,17 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class FindRaceComponent implements OnInit {
 
-  constructor(private router: Router, private api: ApiService) { }
-  //races: number[] = [1, 2, 3, 4, 5, 6];
+  constructor(private router: Router, private api: ApiService, private data: DataService) { }
+
   races: RaceI[] = [];
 
   ngOnInit(): void {
     this.setRaces();
   }
 
-  pickRace() {
+  pickRace(race: any) {
+    this.data.selectedRace = race;
+    console.log(this.data.selectedRace);
     this.router.navigateByUrl("race-inscription");
   }
 
--- StraviaTEC-Web-App/src/app/competitive/race-inscription/race-inscription.component.ts
+++ StraviaTEC-Web-App/src/app/competitive/race-inscription/race-inscription.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { RaceI } from 'src/app/models/race.interface';
+import { ApiService } from 'src/app/services/api.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-race-inscription',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RaceInscriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private data: DataService, private toastr: ToastrService, private api: ApiService) { }
+
+  race!: RaceI;
+  @ViewChild('inputPayment') mapContainer!: ElementRef;
+  isEmpty: boolean = true;
 
   ngOnInit(): void {
+    console.log(this.data.selectedRace);
+    if (this.data.selectedRace) {
+      this.race = this.data.selectedRace;
+    } else {
+      console.log("no data");
+    }
+  }
+
+  confirm() {
+    this.paymentChecker();
+    if (!this.isEmpty) {
+      this.api.joinRace(this.data.currentUser?.userName, this.race.id).subscribe(data => {
+        console.log(data);
+      })
+      this.toastr.success("Successfully inscripted to " + this.race.name + "!", "Success");
+    }
+  }
+
+  paymentChecker() {
+    if (this.mapContainer.nativeElement.files.length == 0) {
+      console.log("no files selected");
+    } else {
+      this.isEmpty = false;
+    }
   }
 
 }
