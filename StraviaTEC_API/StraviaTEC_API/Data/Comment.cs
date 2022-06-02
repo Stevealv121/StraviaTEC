513259cdf907b7abc6369b8e6112161b4937b1c3
Natalia Gonzalez <natigb@estudiantec.cr>
2022-06-02 15:26:31 -0600

    [Fix] Autogenerated comment id

--- StraviaTEC_API/StraviaTEC_API/Data/Comment.cs
+++ StraviaTEC_API/StraviaTEC_API/Data/Comment.cs
@@ -1,11 +1,12 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Comments_API.Data
 {
     public class Comment
     {
         [BsonId]
-        public int id { get; set; }
+        public ObjectId id { get; set; }
         public int activity_id { get; set; }
         public string author { get; set; }
         [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
--- StraviaTEC_API/StraviaTEC_API/Data/CommentsService.cs
+++ StraviaTEC_API/StraviaTEC_API/Data/CommentsService.cs
@@ -17,7 +17,7 @@ namespace Comments_API.Data
             await _comments.Find(_ => true).ToListAsync();
 
         public async Task<Comment> GetComment(int Id) =>
-            await _comments.Find(m => m.id == Id).FirstOrDefaultAsync();
+            await _comments.Find(m => m.id.Equals(Id)).FirstOrDefaultAsync();
 
         public async Task<List<Comment>> GetActivityComments(int activityId) =>
             await _comments.Find(m => m.activity_id == activityId).ToListAsync();
@@ -26,6 +26,6 @@ namespace Comments_API.Data
             await _comments.InsertOneAsync(newComment);
 
         public async Task DeleteComment(int Id) =>
-            await _comments.DeleteOneAsync(m => m.id == Id);
+            await _comments.DeleteOneAsync(m => m.id.Equals(Id));
     }
 }
